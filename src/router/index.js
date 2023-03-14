fb150e6ea1b878f905b3c4c5a6ccf1036fcf9d56
tobi <tygwak@gabia.com>
2023-03-14 15:46:59 +0900

    Test : test

--- src/router/index.js
+++ src/router/index.js
@@ -8,45 +8,45 @@ const router = createRouter({
       path: "/update",
       name: "update",
       component: () => import("../views/UpdateView.vue"),
-      meta : { authorization: ["ROLE_ADMIN"] }
+      // meta : { authorization: ["ROLE_ADMIN"] }
     },
     {
       path: "/create",
       name: "create",
       component: () => import("../views/CreateView.vue"),
-      meta : { authorization: ["ROLE_MEMBER"] }
+      // meta : { authorization: ["ROLE_MEMBER"] }
     },
     {
       path: "/",
       name: "login",
       component: () => import("../views/LoginView.vue"),
-      meta : { authorization: [] }
+      // meta : { authorization: [] }
     },
     {
       path: "/search",
       name: "search",
       component: () => import("../views/SearchView.vue"),
-      meta : { authorization: ["ROLE_MEMBER"] }
+      // meta : { authorization: ["ROLE_MEMBER"] }
     },
   ]
 });
 
-router.beforeEach((to, from, next) => {
-  const { authorization } = to.meta;
-
-  if(authorization.length == 0) {
-    return next();
-  } else {
-    const roles = getRoles();
-    roles.forEach((role) => {
-      if (authorization.includes(role)){
-        return next();
-      }
-    });
-  }
-
-  alert("로그인이 필요합니다.");
-  next({path: "/"});
-});
+// router.beforeEach((to, from, next) => {
+//   const { authorization } = to.meta;
+//
+//   if(authorization.length == 0) {
+//     return next();
+//   } else {
+//     const roles = getRoles();
+//     roles.forEach((role) => {
+//       if (authorization.includes(role)){
+//         return next();
+//       }
+//     });
+//   }
+//
+//   alert("로그인이 필요합니다.");
+//   next({path: "/"});
+// });
 
 export default router;
